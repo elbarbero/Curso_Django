28ca60416cab1816de1fce14d786e03031d73cda
elbarbero <elbarbero400@hotmail.com>
2024-10-24 06:41:22 +0200

    Section 2 finished

--- Seccion 2 - Web Personal/webpersonal/porfolio/views.py
+++ Seccion 2 - Web Personal/webpersonal/porfolio/views.py
@@ -3,6 +3,5 @@ from .models import Project
 
 # Create your views here.
 def porfolio(request):
-    # projects = Project.objects.all()
-    # return render(request=request, template_name="porfolio/porfolio.html", {'projects': projects})
-    return render(request=request, template_name="porfolio/porfolio.html")
+    projects = Project.objects.all()
+    return render(request, "porfolio/porfolio.html", {'projects':projects})
